a51cb6541dd23b8f19c6799e0cbba6e3267d0746
MJS <mjseeger@gmail.com>
2011-12-31 14:38:46 +0100

    Multiple Networks, pre moving filter results to query (currently buggy)

--- code/src/test.py
+++ code/src/test.py
@@ -78,8 +78,8 @@ def decode_contextID(preds, contextID):
     "Converts an integer to the string representation of the corresponding context"
     return list(powerset(sorted(preds)))[contextID]
 
-# TODO: geht das auch ohne preds, d.h. kann die Funktion die preds selbst ausrechnen?
 def insert_local_parameter_sets(con, nwkey, nodes, preds, lpss):
+    # TODO: geht das auch ohne preds, d.h. kann die Funktion die preds selbst ausrechnen?
     for node in sorted(nodes):
         for lps in lpss[node]:
             lpsID = encode_lps(preds[node], lps, base=10)
@@ -188,29 +188,10 @@ if __name__=='__main__':
     networks = {1:'Incoherent type 1 feed-forward'}
 
     # example network: strict, without morphogene
+    # this is graph A in figure 2 of Cotterel/Sharpe
     interactions = {1:{("rr","gg"):"+", ("rr","bb"):"+", ("bb","gg"):"-", ("gg","gg"):"+"}}
     thresholds = {1:{("rr","gg"):2, ("rr","bb"):1, ("bb","gg"):1, ("gg","gg"):1}}
-    filters = {1:
-               {1:["?(rand,mitte: rand.frozen(gg)&rand.max(gg)=0&mitte.frozen(gg)&mitte.min(gg)=1)", None, "AL"], # used to be ...&mitte.max(gg)=1 with the same number of results
-                2:["(rr=2&bb=0&gg=1->EF(AG(gg=0)))&(rr=1&bb=0&gg=1->EF(AG(gg=1)))&(rr=0&bb=0&gg=0->EF(AG(gg=0)))", "forAll", "CTL"]
-                #   (left&middle&right), the gg=1 in left and middle is from the drawing on page 5
-                }}
-    '''
-    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EF(gg=0))", "exists" # 190/202 bei thresholds = 1, obs* # Modell fuer linke Region
-    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&AF(gg=0))", "exists" #   0/202 bei thresholds = 1, obs* # Modell fuer linke Region
-    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EG(gg=0))", "exists" #   0/202 bei thresholds = 1, obs* # Modell fuer linke Region
-    #CTLformula, CTLsearch = "!(rr>0&bb>0&gg>0&EF(gg=0))", "forAll" #  12/202 bei thresholds = 1, obs* # Negation von Modell fuer linke Region 
-    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EF(gg=1))", "exists" # 202/202 bei thresholds = 1, obs* # Modell fuer mittlere Region
-    #CTLformula, CTLsearch = "EF(rr=0&bb=0&gg=1)", "exists" # 202/202 bei thresholds = 1, obs* # Modell fuer rechte Region
-    
-    #mc.filter_extremeAttractors('max', 'attrs', True, True)
-    '''
-    edges = dict(zip(interactions.keys(), [interactions[key].keys() for key in interactions.keys()]))
-    nodes = dict() # will be initialized below
 
-    # this is graph A in figure 2 of Cotterel/Sharpe
-    # including "mm" doubles the number of original parameter sets and the number of filtered parameter sets
-    
     # non-strict, with morphogene
     #interactions = {("mm","rr"):"obs+", ("rr","gg"):"obs+", ("rr","bb"):"obs+", ("bb","gg"):"obs-", ("gg","gg"):"obs+"}
 
@@ -221,38 +202,66 @@ if __name__=='__main__':
     #interactions = {("mm","rr"):"+", ("rr","gg"):"+", ("rr","bb"):"+", ("bb","gg"):"-", ("gg","gg"):"+"}
     #thresholds = {("mm","rr"):1, ("rr","gg"):2, ("rr","bb"):1, ("bb","gg"):1, ("gg","gg"):1}
 
+    networks[2] = 'Two-element negative circuit'
+    interactions[2] = {("X1","X2"):"+", ("X2","X1"):"-"}
+    thresholds[2] = {("X1","X2"):1, ("X2","X1"):1}
+
+    filters = {1:
+               {1:["?(rand,mitte: rand.frozen(gg)&rand.max(gg)=0&mitte.frozen(gg)&mitte.min(gg)=1)", None, "AL"], # used to be ...&mitte.max(gg)=1 with the same number of results
+                2:["(rr=2&bb=0&gg=1->EF(AG(gg=0)))&(rr=1&bb=0&gg=1->EF(AG(gg=1)))&(rr=0&bb=0&gg=0->EF(AG(gg=0)))", "forAll", "CTL"]
+                #   (left&middle&right), the gg=1 in left and middle is from the drawing on page 5
+                }}
+
+    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EF(gg=0))", "exists" # 190/202 bei thresholds = 1, obs* # Modell fuer linke Region
+    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&AF(gg=0))", "exists" #   0/202 bei thresholds = 1, obs* # Modell fuer linke Region
+    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EG(gg=0))", "exists" #   0/202 bei thresholds = 1, obs* # Modell fuer linke Region
+    #CTLformula, CTLsearch = "!(rr>0&bb>0&gg>0&EF(gg=0))", "forAll" #  12/202 bei thresholds = 1, obs* # Negation von Modell fuer linke Region 
+    #CTLformula, CTLsearch = "(rr>0&bb>0&gg>0&EF(gg=1))", "exists" # 202/202 bei thresholds = 1, obs* # Modell fuer mittlere Region
+    #CTLformula, CTLsearch = "EF(rr=0&bb=0&gg=1)", "exists" # 202/202 bei thresholds = 1, obs* # Modell fuer rechte Region
+    
+    #mc.filter_extremeAttractors('max', 'attrs', True, True)
+
+    edges = dict(zip(interactions.keys(), [interactions[key].keys() for key in interactions.keys()]))
+    nodes = dict() # will be initialized below
+    
     # main loop over different regulatory networks:
     for nwkey in networks:
-        print "Considering network:", networks[nwkey]
         print "===================================================================================="
+        print "Considering network:", networks[nwkey]
 
         # set up IG, MC, PSC
         IG = nx.DiGraph()
-        IG.add_edges_from(edges[nwkey])
+        if edges.has_key(nwkey):
+            IG.add_edges_from(edges[nwkey])
+                    
         mc = MC.ModelContainer()
         mc._NuSMVpath = r"C:\Progra~2\NuSMV\2.5.2\bin\NuSMV.exe"
+
         mc.set_IG(IG)
-        mc.set_edgeLabels(interactions[nwkey])
-        mc.set_thresholds(dict((edge, thresholds[nwkey][edge]) for edge in edges[nwkey]))
+        if interactions.has_key(nwkey):
+            mc.set_edgeLabels(interactions[nwkey])
+
+        if thresholds.has_key(nwkey):
+            mc.set_thresholds(dict((edge, thresholds[nwkey][edge]) for edge in edges[nwkey]))
+
         mc.set_initialStates()
         mc.initializePSC()
-        # TODO: replacing the previous by MC.parameterSetup() would allow more flexibility like local constraints etc.
-            
+
+        print "===================================================================================="
+        print "Database operations"
+
         # write network to database
         insert_network(con, nwkey, networks[nwkey])
 
         # write interaction graph to database
         insert_edges(con, nwkey, edges[nwkey], interactions[nwkey], thresholds[nwkey])
 
+        # TODO: replacing the previous by MC.parameterSetup() would allow more flexibility like local constraints etc.
+            
         lpss = mc._psc._localParameterSets
+        #print len(mc._psc)
         nodes[nwkey] = lpss.keys()
         preds = dict([(node, IG.predecessors(node)) for node in nodes[nwkey]]) # dict containing node:[preds of node]
-        
-        # write nodes to database
-        insert_nodes(con, nwkey, nodes[nwkey])
-
-        # write contexts to database
-        insert_contexts(con, nwkey, nodes[nwkey], preds)        
 
         '''
         for node in lpss:
@@ -264,11 +273,16 @@ if __name__=='__main__':
                 print "decoded lpsID:", decode_lps(node, preds[node], encode_lps(preds[node], lps, base=10), base=10)
                 print "========================================="
         '''
+        
+        # write nodes to database
+        insert_nodes(con, nwkey, nodes[nwkey])
+
+        # write contexts to database
+        insert_contexts(con, nwkey, nodes[nwkey], preds)        
 
         # write local parameter sets to database
         insert_local_parameter_sets(con, nwkey, nodes[nwkey], preds, lpss)
         
-        gpss = mc._psc.get_parameterSets()
         # write global parameter sets to database
         # need to re-generate since in python generators cannot be rewound
         gpss = mc._psc.get_parameterSets()
