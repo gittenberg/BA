1e5f175ed7a5a70da6ae5d14c3fc5b9e40db196f
MJS <MJS@MJS-PC2>
2012-03-20 22:23:37 +0100

    Graph isomorphy (work in progress)

--- code/src/graphenumerator.py
+++ code/src/graphenumerator.py
@@ -1,4 +1,5 @@
 import itertools
+import copy
 import numpy as np
 import pickle
 
@@ -32,16 +33,37 @@ def permute_array(MM, perm):
     tempMM2 = np.asarray([tempMM1[:, i] for i in perm])
     return tempMM2
 
-# Later: loop over all networks and remove equivalent ones; recount
+def is_isomorphic(network1, network2):
+    return True
 
-example_network = networks[19000]
-nmr = network_matrix_representation(nodes, example_network)
+comparematrix = np.asarray([[1., -1.,  1.], [1., -1., -1.], [1.,  0.,  0.]])
+unique_networks = copy.copy(networks) # to be reduced
 
-for perm in itertools.permutations(range(len(nodes))):
-    print permute_array(nmr, perm)
+for count1, network1 in enumerate(networks):
+    print "considering network no.", count1
+    nmr1 = network_matrix_representation(nodes, network1)
+    for count2, network2 in enumerate(networks):
+        if count2 > count1:
+            eplus1 = len([edge for edge in network1 if network1[edge]=='+'])
+            eplus2 = len([edge for edge in network2 if network2[edge]=='+'])
+            ezero1 = len([edge for edge in network1 if network1[edge]=='0'])
+            ezero2 = len([edge for edge in network2 if network2[edge]=='0'])
+            if eplus1 == eplus2 and ezero1 == ezero2:
+                for perm in itertools.permutations(range(len(nodes))):
+                    # shuffle matrix belonging to network2 and compare to network1
+                    nmr2 = network_matrix_representation(nodes, network2)
+                    shuffle = permute_array(nmr2, perm)
+                    if np.all(shuffle == nmr1): # the shuffle of network2 equals network1
+                        try:
+                            unique_networks.remove(network2)
+                            networks.remove(network2)
+                        except:
+                            pass
+                        print "network", count2, "is isomorphic to network", count1
 
 # pickle for later use
 ######################################################################
 pickle.dump(networks, file("allnetworks.txt", "w" ))
+pickle.dump(unique_networks, file("unique_networks.txt", "w" ))
 #filecontent = pickle.load(file("allnetworks.txt"))
 #print filecontent
--- code/src/scratch.py
+++ code/src/scratch.py
@@ -1,12 +1,27 @@
+from datetime import datetime
+
+tstart = datetime.now()
+
+import pickle
+import copy
+import itertools
 import numpy as np
 
-#print list(itertools.permutations([1, 2, 3]))
+nets = pickle.load(file("allnetworks.txt"))
+nets = nets[0:400]
+unique_nets = copy.copy(nets)
 
-matrix = [[1, 2, 3],
-          [2, 3, 4],
-          [3, 4, 5]]
+nodes = ["bb", "gg", "rr"]
+labels = ["-", "0", "+"]
 
-permutation = [1, 0, 2]
+def network_matrix_representation(nodelist=None, network=None):
+    '''Returns (node x node) matrix with +1 for activation, -1 for inhibition, 0 for no interaction'''
+    nwmatrep = np.zeros((len(nodelist),len(nodelist)))
+    for mi, m in enumerate(nodelist):
+        for nj, n in enumerate(nodelist):
+            nwmatrep[mi, nj] = labels.index(network[(m, n)]) - 1
+            # here the ordering of labels matters
+    return np.asmatrix(nwmatrep)
 
 def permute_array(MM, perm):
     tempMM0 = np.asarray(MM)
@@ -14,4 +29,31 @@ def permute_array(MM, perm):
     tempMM2 = np.asarray([tempMM1[:, i] for i in perm])
     return tempMM2
 
-print permute_array(matrix, permutation)
+def is_isomorphic(network1, network2):
+    count1 = dict()
+    count2 = dict()
+    for label in labels:
+        count1[label] = len([edge for edge in network1 if network1[edge]==label])
+        count2[label] = len([edge for edge in network2 if network2[edge]==label])
+        if count1[label] != count2[label]: return False
+    nmr1 = network_matrix_representation(nodes, network1)
+    for perm in itertools.permutations(range(len(nodes))):
+        # shuffle matrix belonging to network2 and compare to network1
+        nmr2 = network_matrix_representation(nodes, network2)
+        shuffle = permute_array(nmr2, perm)
+        if np.all(shuffle == nmr1): # the shuffle of network2 equals network1
+            print shuffle - nmr1
+            return True
+    return False
+
+for num1, net1 in enumerate(nets):
+    for num2, net2 in enumerate(nets):
+        if num2 > num1:
+            if is_isomorphic(net1, net2):
+                print "isomorphic:", num1, num2
+                #print len([edge for edge in net if net[edge]=='+'])    
+                #print len([edge for edge in net if net[edge]=='0'])
+                #print len([edge for edge in net if net[edge]=='-'])
+    
+tend = datetime.now()
+print tend-tstart
