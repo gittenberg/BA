e9828fb64d89aa7912e1747b2ed7ef214d676096
MJS <MJS@MJS-PC2>
2012-05-15 21:25:27 +0200

    Counting and splitting global parameter sets

---
+++ code/_08_STG_reducer.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+tstart = datetime.now()
+
+import cPickle
+from _02_regnet_generator import dict_to_model
+from _03_database_functions import encode_gps
+
+
+def subparset(parset, is_m1_in, is_m2_in):
+    return {key:{tuple(y for y in context if y!='m1' and y!='m2'):parset[key][context] 
+                      for context in parset[key].keys() 
+                      if key != 'rr' or ('m1' in context) == is_m1_in and ('m2' in context) == is_m2_in} 
+                      for key in parset.keys() if key != 'm1' and key != 'm2'}
+    
+
+if __name__=='__main__':
+    mode = "with_morphogene"
+    if mode=="with_morphogene":
+        add_morphogene=True
+    elif mode=="without_morphogene":
+        add_morphogene=False
+    else:
+        print "warning: morphogene mode not set."
+    pstotal = 0
+    graphcount = 0
+    
+    combis = [(False, False), (True, False), (False, True)] # low, medium, high
+    
+    picklename = "connected_unique_networks_three_nodes_"+mode+".db"
+    networks = cPickle.load(file(picklename))
+    print "found", len(networks), "networks."
+    split_gps_store = dict() # will be {nwkey:[encoding_of_split_gps1, ..., encoding_of_split_gps3]}
+    
+    allsubgpss = set()
+    for nwkey in networks:
+        #if nwkey >= 2: continue # enable for quick check
+        print "===================================================================================="
+        print "considering nwkey:", nwkey
+        #print networks[nwkey]
+
+        mc = dict_to_model(networks[nwkey], add_morphogene)
+        npsc = len(mc._psc)
+        print nwkey, ":", npsc, "parameter sets."
+        pstotal += npsc
+        print nwkey, ":", pstotal, "parameter sets in total."
+        gpss = mc._psc.get_parameterSets()
+        for gps in gpss:
+            for combi in combis:
+                #print subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1])
+                subgps = encode_gps(subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1]))
+                allsubgpss.add(subgps)
+            #print gps
+            #print encode_gps(gps)
+        print len(allsubgpss)
+        split_gps_store[nwkey] = [npsc, pstotal, len(allsubgpss)]
+
+        tend = datetime.now()
+        print "total execution time:", tend-tstart
+    
+    print "pickling results to: split_gps_store.pkl"
+    cPickle.dump(split_gps_store, file("split_gps_store.pkl", "w"))
+    print "done."
+    
--- code/_scratch.py
+++ code/_scratch.py
@@ -1,19 +1,5 @@
+import cPickle
 
-parset = {'gg': {('gg', 'rr'): 2, ('bb', 'rr'): 1, ('bb', 'gg', 'rr'): 0, ('gg',): 1, ('rr',): 1, (): 1, ('bb', 'gg'): 1, ('bb',): 1}, 
-          'm1': {(): 0, ('m1',): 1}, 
-          'rr': {('bb', 'gg', 'm1', 'm2'): 1, ('bb', 'm2', 'rr'): 1, ('bb', 'gg', 'm1'): 1, ('bb', 'm1'): 1, ('bb', 'gg', 'm2'): 1, ('rr',): 1, ('gg', 'm1', 'rr'): 1, ('gg', 'm1', 'm2'): 1, ('bb', 'rr'): 1, ('m2',): 1, ('gg', 'm1', 'm2', 'rr'): 1, ('bb', 'gg', 'rr'): 0, ('gg', 'm2'): 1, ('gg',): 1, ('bb', 'gg', 'm1', 'm2', 'rr'): 1, ('bb',): 1, ('gg', 'm2', 'rr'): 1, ('bb', 'gg', 'm1', 'rr'): 1, ('bb', 'gg'): 1, ('m1', 'm2', 'rr'): 1, ('bb', 'm1', 'm2', 'rr'): 1, ('bb', 'gg', 'm2', 'rr'): 1, ('gg', 'm1'): 1, ('bb', 'm1', 'm2'): 1, ('m2', 'rr'): 1, ('m1',): 1, ('gg', 'rr'): 1, ('m1', 'm2'): 1, ('bb', 'm1', 'rr'): 1, (): 1, ('bb', 'm2'): 1, ('m1', 'rr'): 1}, 
-          'bb': {('gg', 'rr'): 1, ('bb', 'rr'): 1, ('bb', 'gg', 'rr'): 0, ('gg',): 1, ('rr',): 1, (): 1, ('bb', 'gg'): 1, ('bb',): 1}, 
-          'm2': {('m2',): 1, (): 0}}
+split_gps_store = cPickle.load(file("split_gps_store.pkl"))
 
-
-def subparset(parset, is_m1_in, is_m2_in):
-    return {key:{context:parset[key][context] for context in parset[key].keys() if key!='rr' or (('m1' in context)==is_m1_in and ('m2' in context)==is_m2_in)} for key in parset.keys() if key!='m1' and key!='m2'}
-
-#subparset = {key:{context:parset[key][context] for context in parset[key].keys()} for key in parset.keys()}
-
-tmp = subparset(parset, False, True)
-
-print tmp==parset
-
-for x in tmp.items():
-    print x
+print split_gps_store
