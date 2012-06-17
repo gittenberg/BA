c7e236d3876805d6f77ad19bc90870ad1686723e
MJS <MJS@MJS-PC2>
2012-06-17 17:40:50 +0200

    0000 - 0999 merging

--- code/_11_gps_combiner.py
+++ code/_11_gps_combiner.py
@@ -24,8 +24,12 @@ if __name__=='__main__':
         print "warning: morphogene mode not set."
     
     combis = [(False, False), (True, False), (False, True)] # low, medium, high
-    shelvefilename = "small_gps_pass_test.db"
-    d = shelve.open(shelvefilename)    
+    small_gps_pass_shelvename = "small_gps_pass_test.db"
+    d = shelve.open(small_gps_pass_shelvename)    
+    combined_results_shelvename = "combined_results.db"
+    crs = shelve.open(combined_results_shelvename)
+    passing_sets_shelvename = "passing_sets.db"
+    pss = shelve.open(passing_sets_shelvename)
 
     networks = cPickle.load(file(picklename))
     tocheck = len(networks)
@@ -35,7 +39,7 @@ if __name__=='__main__':
     current = 0
     for nwkey in networks:
         current += 1
-        if nwkey==0 or nwkey >= 2: continue # enable for quick check
+        if nwkey >= 1000: continue # enable for quick check
         print "===================================================================================="
         print "considering nwkey:", nwkey
         
@@ -46,22 +50,39 @@ if __name__=='__main__':
         print nwkey, ":", pstotal, "parameter sets in total."
         
         gpss = mc._psc.get_parameterSets()
+        count_exists = 0
+        count_forall = 0
+        pss_exists = []
+        pss_forall = []
         for gps in gpss:
             # decompose gps code into 3 small gps codes
-            #subgps = dict(combi:encode_gps_full(subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1])) for combi in combis)
-            subgps = dict(combi:4 for combi in combis)
-            # FIXME: syntax fehler reparieren
-            # combine_truth_values einbauen
-            # loop loeschen
-            # dann kann's losgehen mit dem hochzaehlen
-            print combine_truth_values()
-            for combi in combis:
-                subgps[combi] = encode_gps_full(subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1]))
-                print combi, subgps[combi], d[subgps[combi]]
-                # schau die einzelteile nach und merke dir, zu welchem aus lo, mid, high sie gehoeren
-                # kombiniere die 0, 1 werte zu einem
-                
+            subgps = dict((combi, encode_gps_full(subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1]))) for combi in combis)
+            #d[subgps[combis[0]]]: left
+            #d[subgps[combis[1]]]: middle
+            #d[subgps[combis[2]]]: right
+            exists = d[subgps[combis[0]]][0] and d[subgps[combis[1]]][2] and d[subgps[combis[2]]][0] # EFAG
+            forall = d[subgps[combis[0]]][1] and d[subgps[combis[1]]][3] and d[subgps[combis[2]]][1] # AFAG
+            #print exists, forall
+            if exists==1:
+                pss_exists.append(gps)
+                count_exists += 1
+            if forall==1:
+                pss_forall.append(gps)
+                count_forall += 1
+            #for combi in combis:
+                #subgps[combi] = encode_gps_full(subparset(gps, is_m1_in=combi[0], is_m2_in=combi[1]))
+                #print combi, subgps[combi], d[subgps[combi]]
+        results = [count_exists, count_forall, npsc, count_exists*1.0/npsc, count_forall*1.0/npsc]
+        print results
+        crs[str(nwkey)] = results
+        passing_sets = [pss_exists, pss_forall]
+        #print passing_sets
+        pss[str(nwkey)] = passing_sets
         tend = datetime.now()
         print "total execution time:", tend - tstart
         print "expected finishing time:", tstart + (tend - tstart) * tocheck / current
-        
+    
+    d.close()
+    crs.close()
+    pss.close()
+    print "done."
--- code/_scratch.py
+++ code/_scratch.py
@@ -1,17 +1,26 @@
-import cPickle
+import shelve
 
-d1 = [1, 0, 1, 1]
-d2 = [1, 0, 1, 0]
-d3 = [1, 0, 0, 1]
+shname = "test.db"
 
-print d1
-print d2
+d = shelve.open(shname)
 
-print (d1 and d2) and d3 # unexpected!!
-print [d1[i] and d2[i] and d3[i] for i in range(4)] # is what we want!
+d["0"] = [1, 0, 0, 1]
+d["1"] = [1, 0, 1, 1]
+d["2"] = [1, 0, 1, 0]
+
+d.close()
+
+dd = shelve.open(shname)
+
+
+#print (d1 and d2) and d3 # unexpected!!
+#print [d1[i] and d2[i] and d3[i] for i in range(4)] # is what we want!
 
 def combine_truth_values(*args):
     ll = len(args[0])
     return [all([arg[i] for arg in args])*1 for i in range(ll)]
 
-print combine_truth_values(d1, d2, d3)
+print combine_truth_values(dd["0"], dd["1"], dd["2"])
+
+print dd["0"], dd["1"], dd["2"]
+
