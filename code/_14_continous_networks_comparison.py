72295e7624011fc67fa1d1a1c740aeb4e6c1d12b
MJS <MJS@MJS-PC2>
2012-06-23 22:19:03 +0200

    Update (buggy)

--- code/_14_continous_networks_comparison.py
+++ code/_14_continous_networks_comparison.py
@@ -1,11 +1,8 @@
 
 import shelve
 import cPickle
-#from _01_graph_enumerator import backup, generate_all_networks
-#from _02_regnet_generator import dict_to_model
-
-nodes = ["bb", "gg", "rr"]
-labels = ["-", "+"]
+from _02_regnet_generator import dict_to_model
+from _12_AL_model_checker import filter_byAL
 
 def remove_zero_edges(networks):
     print "found", len(networks), "networks.\nremoving null edges...",
@@ -34,7 +31,7 @@ def lookup_iso_rep(number):
 
 if __name__=='__main__':
     networks = dict()
-    for letter in 'ABDEF': # in C, green is the input gene
+    for letter in 'ABDEF': # in C, unfortunately green is the input gene
         networks[letter] = dict()
     
     networks['A']['name'] = '(A) Incoherent type 1 feed-forward'
@@ -62,13 +59,30 @@ if __name__=='__main__':
     #print isoclasses
     
     for crsname in combined_results_shelvenames:
-        print "using results from:", crsname
         print "========================================="
+        print "using results from:", crsname
         crs = shelve.open(crsname)
         for net in networks:
-            print net#, networks[net]['interactions']
+            print net #, networks[net]['interactions']
             graph_number = lookup_graph_number(networks[net]['interactions'])
-            #print graph_number 
             iso_graph_number = lookup_iso_rep(graph_number) 
+            print iso_graph_number 
             print crs[str(iso_graph_number)]
             #print dict_to_model(networks[net]['interactions'], add_morphogene=True)
+
+    print "========================================="
+    print "AL model checking"
+    crs = shelve.open(combined_results_shelvenames[0])
+    numbers = [7747, 7423, 7969, 9349, 14284] # TODO: this should be generated in the above loop (simple)
+    # I see where the problem is: gg=1 can be in everywhere, despite the suggestive name...
+    ALformula = "(?(left: left.frozen(gg)&left.max(gg)=0)) & (?(middle: middle.frozen(gg)&middle.min(gg)=1)) & (?(right: right.frozen(gg)&right.max(gg)=0))"
+    #ALformula = "(?(left, right: left.frozen(gg)&left.max(gg)=0&right.frozen(gg)&right.max(gg)=0)) & (?(middle: middle.frozen(gg)&middle.min(gg)=1))"
+    for number in numbers:
+        mc = dict_to_model(allnetworks[number], add_morphogene=True)
+        gpss = mc._psc.get_parameterSets()
+        count_accepted = 0
+        for gps in gpss:
+            #print gps
+            accepted = filter_byAL(mc, gps, ALformula)
+            print accepted
+        
